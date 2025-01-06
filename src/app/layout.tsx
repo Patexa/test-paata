80c6b14e1e76a400ded0ee7135bbb5b992e58f58
Patexa <20100261@ibsu.edu.ge>
2025-01-06 16:35:29 +0400

    feat: added variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import React from 'react';
 
+const a = 5;
+console.log(a);
+
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
